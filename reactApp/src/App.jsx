c58bd1c3d288f86d04c5df58d4d18db146fe8cdb
unknown <njanadams@gmail.com>
2024-04-23 21:36:55 +0530

    added sidebar and homepage video

---
+++ reactApp/src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from './NavBar/NavBar'
+import './App.css'
+import MainPage from './mainPage/mainPage'
+
+function App() {
+  
+
+  return (
+    <>
+    <NavBar/>
+    <MainPage/>
+    </>
+  )
+}
+
+export default App
