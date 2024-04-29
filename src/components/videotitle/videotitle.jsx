338e8d01b8e4741af0685d38289a28b3d8630f01
StackVishnu <you@example.com>
2024-04-29 10:00:08 +0530

    test

--- src/components/videotitle/videotitle.jsx
+++ src/components/videotitle/videotitle.jsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-<<<<<<< HEAD
 function VideoTitle(){
      return(
         <>
@@ -30,35 +29,6 @@ function VideoTitle(){
        
         </>
      )
-=======
-function VideoTitle() {
-  return (
-    <>
-      <div className="title">
-        <img src={titleCard} alt="image" className="title-card" />
-        <h4 className="movie-specs">
-          2012 <span className="dot"></span> 2h24m <span classname="dot"></span>{" "}
-          4 languages <span classname="dot"></span> U/A 16+
-        </h4>
-      </div>
-      <div className="description">
-        <div className="description-text">
-          <p>
-            Earth's mightiest heroes must come together and learn to fight as a
-            team if they are going to stop the mischievous Loki and his alien
-            army from enslaving humanity.
-          </p>
-          <h4>Superhero | Action | Dark Humour</h4>
-        </div>
-        <div className="subscribe-button">
-          <button className="button-4" role="button">
-            <FontAwesomeIcon
-              icon={faPlay}
-              style={{ color: "#ffffff", marginRight: "1rem" }}
-            />
-            Subscribe to Watch
-          </button>
->>>>>>> e1260a1d124828b4e8a2efefb535f77474bb43ad
 
           <button className="button-5" role="button">
             <FontAwesomeIcon icon={faPlus} style={{ color: "#ffffff" }} />
